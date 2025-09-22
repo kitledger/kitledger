b75126e51f9bfafa233d23be8c4ed855fba98ec7
Alejandro Barrera Aponte <abarrera@entropy.cl>
2025-09-22 18:35:42 -0300

    Working without Pool

--- src/main/kotlin/services/database/Database.kt
+++ src/main/kotlin/services/database/Database.kt
@@ -1,27 +1,16 @@
 package com.kitledger.services.database
 
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
-import io.r2dbc.pool.ConnectionPool
-import io.r2dbc.pool.ConnectionPoolConfiguration
-import io.r2dbc.spi.ConnectionFactories
 import com.kitledger.services.config.AppConfig
-import java.time.Duration
 
 object DatabaseFactory {
     fun init() {
         val config = AppConfig.dbConfig
         val dbUrl = if (config.ssl) "${config.url}?ssl=true" else config.url
-        val connectionFactory = ConnectionFactories.get(dbUrl)
 
-        val poolConfiguration = ConnectionPoolConfiguration.builder(connectionFactory)
-            .maxSize(config.max)
-            .initialSize(5)
-            .maxIdleTime(Duration.ofMinutes(10))
-            .validationQuery("SELECT 1")
-            .build()
-
-        val pool = ConnectionPool(poolConfiguration)
-
-        R2dbcDatabase.connect { pool }
+        R2dbcDatabase.connect(
+            url = dbUrl,
+            driver = "postgresql",
+        )
     }
 }
