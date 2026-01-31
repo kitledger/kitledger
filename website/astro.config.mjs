f4709f5f4eb659b2e0c92310a7ccff9efddf3efc
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2026-01-31 19:55:32 -0300

    Vitepress

--- website/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [mdx()]
-});
